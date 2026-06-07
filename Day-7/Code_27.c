b77a38bf60f29465b7530585d50891c5c33db2be
25113cn290-dpsharma <25113cn290@glbitm.ac.in>
2026-06-07 11:34:49 +0530

    Day-7 uploaded

---
+++ Day-7/Code_27.c
@@ -0,0 +1,20 @@
+// Write a program to Recursive sum of digits.
+
+#include <stdio.h>
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+        return (n % 10) + sum(n / 10);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    printf("Sum of digits = %d\n", sum(num));
+    return 0;
+}
